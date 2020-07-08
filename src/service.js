6a1ec47d436b972cd4f1000eb441816f5d0e6b54
Kairosds <kairosds@MBP-de-Kairosds.home>
2020-07-09 00:20:32 +0200

    feat/6-service: separate methods

--- src/service.js
+++ src/service.js
@@ -6,49 +6,28 @@ const token = 'hjgfdskadfhb293u284u238u423'
 export function verifyUserRegistered (username, password) {
   const userLogged = registeredUsers.filter(user => user.username === username && user.password === password)
   if (userLogged.length === 0) {
-    new Promise((resolve, reject) => {
-      setTimeout(function () {
-        reject(new Error('Unauthorized'))
-      }, 1000)
-    }).catch(function (err) {
-      console.error(err)
-    })
+    loginError()
   } else {
-    new Promise((resolve) => {
-      setTimeout(function () {
-        resolve(token)
-      }, 1000)
-    }).then(function (result) {
-      console.log('?????????', result)
-    })
+    loginSuccess()
   }
 }
 
-// export function verifyUserRegistered (username, password) {
-//   const userLogged = registeredUsers.filter(user => user.username === username && user.password === password)
-//   if (userLogged.length === 0) {
-//     return loginError
-//   } else {
-//     return loginSuccess
-//   }
-// }
+function loginError () {
+  new Promise((resolve, reject) => {
+    setTimeout(function () {
+      reject(new Error('Unauthorized'))
+    }, 1000)
+  }).catch(function (err) {
+    console.error(err)
+  })
+}
 
-// const loginSuccess = new Promise((resolve) => {
-//   setTimeout(function () {
-//     resolve(token)
-//   }, 1000)
-// })
-
-// const loginError = new Promise((resolve, reject) => {
-//   setTimeout(function () {
-//     reject(new Error('Unauthorized'))
-//   }, 1000)
-// })
-
-// loginSuccess.then(function (result) {
-//   console.log('token', result)
-// })
-
-// loginError.catch(function (err) {
-//   console.log(err)
-// })
+function loginSuccess () {
+  new Promise((resolve) => {
+    setTimeout(function () {
+      resolve(token)
+    }, 1000)
+  }).then(function (result) {
+    localStorage.setItem('token', result)
+  })
+}

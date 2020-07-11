a8c17d72e18db0bbc1fbc9f9e851645c5c21b875
Kairosds <kairosds@MacBook-Pro-de-Kairosds.local>
2020-07-11 09:50:41 +0200

    feat/6-service: test settimeout login success

--- tests/unit/service.spec.js
+++ tests/unit/service.spec.js
@@ -1,5 +1,5 @@
 import registeredUsers from '../../src/registeredUsers.json'
-import { verifyUserRegistered } from '../../src/service'
+import { verifyUserRegistered, loginSuccess } from '../../src/service'
 describe('service', () => {
   test('verifyUserRegistered should verify is user is registered by username and password', () => {
     const username = 'pepa'
@@ -8,5 +8,15 @@ describe('service', () => {
     const userLogged = registeredUsers.filter(user => user.username === username && user.password === password)
     verifyUserRegistered()
     expect(userLogged).toStrictEqual(credentials)
+  })
+  test('loginSuccess should call setTimeOut', () => {
+    jest.useFakeTimers()
+
+    loginSuccess()
+
+    expect(setTimeout).toHaveBeenCalledTimes(1)
+  })
+  test('when login should return token', () => {
+    
   })
 })

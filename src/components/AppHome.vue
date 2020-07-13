4fa3bbb67147f571e188576b010eaccabb31b6f3
Kairosds <kairosds@MacBook-Pro-de-Kairosds.local>
2020-07-13 17:55:44 +0200

    feat/7-logout: logout functionality and AppHome component

---
+++ src/components/AppHome.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <button @click="logout">logout</button>
+      Welcome to the app!
+     <router-view/>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  name: 'AppHome',
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    const tokenStored = localStorage.getItem('token')
+    if (!tokenStored) {
+      window.location.href = '/'
+    }
+  },
+  methods: {
+    logout () {
+      localStorage.removeItem('token')
+      window.location.href = '/'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
